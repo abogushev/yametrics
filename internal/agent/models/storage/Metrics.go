dcfdc600326d9f235f0e494f170aa91a9c485b42
a.bogushev <a.bogushev@tinkoff.ru>
2022-11-04 13:09:37 +0300

    inc-17 add  MetricToMap benchmark

--- internal/agent/models/storage/Metrics.go
+++ internal/agent/models/storage/Metrics.go
@@ -88,3 +88,44 @@ func (m *Metrics) MetricToMaps() (map[string]float64, map[string]int64) {
 
 	return m2gauge, m2counter
 }
+
+
+func (m *Metrics) MetricToMapsOld() (map[string]float64, map[string]int64) {
+    	m2gauge := make(map[string]float64)
+    m2gauge["Alloc"] = float64(m.Alloc)
+    m2gauge["BuckHashSys"] = float64(m.BuckHashSys)
+    m2gauge["Frees"] = float64(m.Frees)
+    m2gauge["GCCPUFraction"] = m.GCCPUFraction
+    m2gauge["GCSys"] = float64(m.GCSys)
+    m2gauge["HeapAlloc"] = float64(m.HeapAlloc)
+    m2gauge["HeapIdle"] = float64(m.HeapIdle)
+    m2gauge["HeapInuse"] = float64(m.HeapInuse)
+    m2gauge["HeapObjects"] = float64(m.HeapObjects)
+    m2gauge["HeapReleased"] = float64(m.HeapReleased)
+    m2gauge["HeapSys"] = float64(m.HeapSys)
+    m2gauge["LastGC"] = float64(m.LastGC)
+    m2gauge["Lookups"] = float64(m.Lookups)
+    m2gauge["MCacheInuse"] = float64(m.MCacheInuse)
+    m2gauge["MCacheSys"] = float64(m.MCacheSys)
+    m2gauge["MSpanInuse"] = float64(m.MSpanInuse)
+    m2gauge["MSpanSys"] = float64(m.MSpanSys)
+    m2gauge["Mallocs"] = float64(m.Mallocs)
+    m2gauge["NextGC"] = float64(m.NextGC)
+    m2gauge["NumForcedGC"] = float64(m.NumForcedGC)
+    m2gauge["NumGC"] = float64(m.NumGC)
+    m2gauge["OtherSys"] = float64(m.OtherSys)
+    m2gauge["PauseTotalNs"] = float64(m.PauseTotalNs)
+    m2gauge["StackInuse"] = float64(m.StackInuse)
+    m2gauge["StackSys"] = float64(m.StackSys)
+    m2gauge["Sys"] = float64(m.Sys)
+    m2gauge["TotalAlloc"] = float64(m.TotalAlloc)
+    m2gauge["RandomValue"] = m.RandomValue
+    m2gauge["TotalMemory"] = m.TotalMemory
+    m2gauge["FreeMemory"] = m.FreeMemory
+    m2gauge["CPUutilization1"] = m.CPUutilization1
+
+    	m2counter := make(map[string]int64)
+    m2counter["PollCount"] = m.PollCount
+
+    return m2gauge, m2counter
+}
---
+++ internal/agent/models/storage/metrics_test.go
@@ -0,0 +1,21 @@
+package storage
+
+import (
+    "runtime"
+    "testing"
+)
+var M = &Metrics{MemStats: &runtime.MemStats{}, PollCount: 0, RandomValue: 0.0}
+
+func BenchmarkMetricToMap(b *testing.B) {
+    b.Run("current", func(b *testing.B) {
+        for i := 0; i < b.N; i++ {
+            M.MetricToMaps()
+        }
+    })
+    b.Run("old", func(b *testing.B) {
+        for i := 0; i < b.N; i++ {
+            M.MetricToMapsOld()
+        }
+    })
+
+}
